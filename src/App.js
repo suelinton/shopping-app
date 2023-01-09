50273aaf3e2680c79b9c58aa9eb99af7cd907526
Suelinton Farias <suelinton.farias-ext@viavarejo.com.br>
2023-01-09 05:43:34 -0300

    refactor: removido teste unitário..

--- src/App.js
+++ src/App.js
@@ -12,14 +12,6 @@ function App() {
         </BrowserRouter>
         <Toast />
       </ProvideCart>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </div>
   );
 }
